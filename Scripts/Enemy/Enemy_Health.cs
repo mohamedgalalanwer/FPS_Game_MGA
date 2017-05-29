da48adc5ba1e868cf164743efbf4666b3a5e96be
Mohamed Galal <abogalalmedo@gmail.com>
2017-05-29 20:07:11 +0200

    Scripts

---
+++ Scripts/Enemy/Enemy_Health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Health : MonoBehaviour {
+
+    private Enemy_Master enemyMaster;
+    public int enemyHealth = 100;
+    void SetIntialReferance()
+    {
+        enemyMaster = GetComponent<Enemy_Master>();
+
+    }
+    private void OnEnable()
+    {
+        SetIntialReferance();
+        enemyMaster.EventEnemyDedectHealth += DeductionHealth;
+    }
+    private void OnDisable()
+    {
+        enemyMaster.EventEnemyDedectHealth -= DeductionHealth;
+    }
+    private void DeductionHealth(int health)
+    {
+        enemyHealth -= health;
+        if (enemyHealth < 0)
+        {
+            enemyHealth = 0;
+            enemyMaster.CallEventEnemyDie();
+            Destroy(gameObject, UnityEngine.Random.Range(10, 20));
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Scripts/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+
+    // put the script on  eny enemy 
+
+
+    [SerializeField] private float health = 100;	// Use this for initialization
+	public void ApplyDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject,0.1f);
+        }
+        //you can put the animation death of the eniems here 
+
+
+
+    }
+}
